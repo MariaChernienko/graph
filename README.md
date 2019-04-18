c84be2f9d049b022ba3f90a43660c130e6c94cfd
Maria <murikmarusia@gmail.com>
2019-04-18 09:56:41 +0300

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# graph
+# Real time graph
+
+[Watch demo](https://arcane-falls-82498.herokuapp.com/)
+
+Real-time graph, which is updated every second on request to the server. The client part is implemented with React using create-react-app. The server part is implemented using nodejs + express, posted on [Heroku: Cloud Application Platform](https://www.heroku.com/)
--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-
-
-
-
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  res.send(Math.random().toString());
-
-  console.log(`Sent passwords`);
+app.get("/api/passwords", (req, res) => {
+  const rand = Math.random().toString();
+  res.send(rand);
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
-
-
-
-
-
+console.log(`Random number generator listening on ${port}`);
